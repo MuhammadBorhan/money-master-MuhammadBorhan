7ecc84718956616e17daa46f65a78f1af6b884f1
Muhammad Borhan <0317371981@student.wub.edu.bd>
2022-02-17 09:33:24 +0600

    function added

--- js/index.js
+++ js/index.js
@@ -7,30 +7,34 @@ function getInputField(input) {
     return inputBalance;
 }
 
-// calculate income and expenses amount using event handler
-document.getElementById('calculate-button').addEventListener('click', function () {
-    const incomeAmount = getInputField('income-input');
-    const foodAmount = getInputField('food-input');
-    const rentAmount = getInputField('rent-input');
-    const clothesAmount = getInputField('clothes-input');
+function getButton(commonButton) {
+    document.getElementById(commonButton).addEventListener('click', function () {
 
-    const totalExpenses = foodAmount + rentAmount + clothesAmount;
-    document.getElementById('total-expns').innerText = totalExpenses;
+        // Calculate income and Expenses amount
+        const incomeAmount = getInputField('income-input');
+        const foodAmount = getInputField('food-input');
+        const rentAmount = getInputField('rent-input');
+        const clothesAmount = getInputField('clothes-input');
 
-    const balance = incomeAmount - totalExpenses;
-    document.getElementById('balance').innerText = balance;
+        const totalExpenses = foodAmount + rentAmount + clothesAmount;
+        document.getElementById('total-expns').innerText = totalExpenses;
 
-});
+        const balance = incomeAmount - totalExpenses;
+        document.getElementById('balance').innerText = balance;
 
+        // Calculate Saving amount and Remaining balance
+        const saveInputValue = getInputField('save-input');
+        const balance2 = document.getElementById('balance').innerText;
 
-// saving amount and remaining balance calculate using event handler
-document.getElementById('save-button').addEventListener('click', function () {
-    const saveInputValue = getInputField('save-input');
+        if (isNaN(saveInputValue)) {
 
-    const balance = document.getElementById('balance').innerText;
-    const savingBalance = (balance * saveInputValue) / 100;
-    document.getElementById('saving-amount').innerText = savingBalance;
-
-    const remainingBalance = balance - savingBalance;
-    document.getElementById('remaining-bal').innerText = remainingBalance;
-});
+        } else {
+            const savingBalance = (balance2 * saveInputValue) / 100;
+            document.getElementById('saving-amount').innerText = savingBalance;
+            const remainingBalance = balance2 - savingBalance;
+            document.getElementById('remaining-bal').innerText = remainingBalance;
+        }
+    });
+}
+getButton('calculate-button');
+getButton('save-button');
